006c361207951ff8e35fd4ce8c036035572b0692
yuliu2016 <22801773+yuliu2016@users.noreply.github.com>
2019-08-10 21:43:13 -0400

    fix knotCameraTest

--- core/camera/src/main/java/knotbook/core/camera/KnotCameraTest.java
+++ core/camera/src/main/java/knotbook/core/camera/KnotCameraTest.java
@@ -4,19 +4,30 @@ import com.github.sarxos.webcam.Webcam;
 import com.github.sarxos.webcam.ds.buildin.WebcamDefaultDriver;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class KnotCameraTest {
 
     public static void test() {
         Webcam.setDriver(new WebcamDefaultDriver());
-//        System.out.println(Webcam.getDefault());
-        KnotCamera camera = new KnotCamera();
+        KnotCamera camera;
+        try {
+            camera = new KnotCamera();
+        } catch (Exception e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR, e.getMessage());
+            alert.show();
+            return;
+        }
 
         Stage stage = new Stage();
+        stage.setTitle("Camera Test");
 
         VBox box = new VBox();
 
@@ -26,7 +37,14 @@ public class KnotCameraTest {
         view.setFitHeight(720.0);
 
         Label label = new Label();
-        label.setStyle("-fx-text-fill: #0a0");
+        label.setStyle("-fx-text-fill: white");
+        camera.resultProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue == null) {
+                box.setBackground(null);
+            } else {
+                box.setBackground(new Background(new BackgroundFill(Color.rgb(0, 169, 0), null, null)));
+            }
+        });
         label.textProperty().bind(camera.resultProperty());
 
         box.getChildren().addAll(view, label);
