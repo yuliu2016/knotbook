2c1fecf0b59ed0e8d197e4b2c80b6e1721b50b92
yuliu2016 <22801773+yuliu2016@users.noreply.github.com>
2019-08-15 13:35:12 -0400

    uncomment

--- core/table/src/main/java/knotbook/core/table/KnotableSkin.kt
+++ core/table/src/main/java/knotbook/core/table/KnotableSkin.kt
@@ -37,20 +37,20 @@ class KnotableSkin(knotable: Knotable) : SkinBase<Knotable>(knotable) {
         max = 1.0
     }
 
-//    val visualBounds: Rectangle2D = Screen.getPrimary().visualBounds
-//
-//    val vln: List<Line>
-//    val hln: List<Line>
-//
-//    val cells: List<Text>
+    val visualBounds: Rectangle2D = Screen.getPrimary().visualBounds
+
+    val vln: List<Line>
+    val hln: List<Line>
+
+    val cells: List<Text>
 
     init {
-//        vln = (0..(visualBounds.width / kMinCellWidth).toInt()).map { Line() }
-//        hln = (0..(visualBounds.width / kMinCellHeight).toInt()).map { Line() }
-//        cells = (0 until vln.size * hln.size).map { Text("0") }
-//        children.addAll(vln)
-//        children.addAll(hln)
-//        children.addAll(cells)
+        vln = (0..(visualBounds.width / kMinCellWidth).toInt()).map { Line() }
+        hln = (0..(visualBounds.width / kMinCellHeight).toInt()).map { Line() }
+        cells = (0 until vln.size * hln.size).map { Text("0") }
+        children.addAll(vln)
+        children.addAll(hln)
+        children.addAll(cells)
         children.addAll(hsb, vsb)
     }
 
@@ -68,43 +68,43 @@ class KnotableSkin(knotable: Knotable) : SkinBase<Knotable>(knotable) {
 
     @Suppress("DuplicatedCode")
     override fun layoutChildren(contentX: Double, contentY: Double, contentWidth: Double, contentHeight: Double) {
-//        for (i in 0 until vln.size) {
-//            val line = vln[i]
-//            line.startX = 0.0
-//            line.startY = contentHeight
-//            line.endX = 0.0
-//            line.endY = 0.0
-//            line.strokeWidth = kLineStroke
-//            line.stroke = Color.GRAY
-//            val lineX = contentX + i * kMinCellWidth
-//            layoutInArea(line, lineX, contentY,
-//                    kMinCellWidth, contentHeight, -0.0,
-//                    HPos.LEFT, VPos.TOP)
-//        }
-//
-//        for (j in 0 until hln.size) {
-//            val line = hln[j]
-//            line.startX = contentWidth
-//            line.startY = 0.0
-//            line.endX = 0.0
-//            line.endY = 0.0
-//            line.strokeWidth = kLineStroke
-//            line.stroke = Color.GRAY
-//            val lineY = contentY + j * kMinCellHeight
-//            layoutInArea(line, contentX, lineY,
-//                    contentWidth, kMinCellHeight, -0.0,
-//                    HPos.LEFT, VPos.TOP)
-//        }
-//
-//        for (i in 0 until vln.size) {
-//            for (j in 0 until hln.size) {
-//                val cell = cells[i * hln.size + j]
-//                val lineX = contentX + i * kMinCellWidth + kTextMargin
-//                val lineY = contentY + j * kMinCellHeight
-//                layoutInArea(cell, lineX, lineY, kMinCellWidth - 2 * kTextMargin,
-//                        kMinCellHeight, 0.0, HPos.LEFT, VPos.CENTER)
-//            }
-//        }
+        for (i in 0 until vln.size) {
+            val line = vln[i]
+            line.startX = 0.0
+            line.startY = contentHeight
+            line.endX = 0.0
+            line.endY = 0.0
+            line.strokeWidth = kLineStroke
+            line.stroke = Color.GRAY
+            val lineX = contentX + i * kMinCellWidth
+            layoutInArea(line, lineX, contentY,
+                    kMinCellWidth, contentHeight, -0.0,
+                    HPos.LEFT, VPos.TOP)
+        }
+
+        for (j in 0 until hln.size) {
+            val line = hln[j]
+            line.startX = contentWidth
+            line.startY = 0.0
+            line.endX = 0.0
+            line.endY = 0.0
+            line.strokeWidth = kLineStroke
+            line.stroke = Color.GRAY
+            val lineY = contentY + j * kMinCellHeight
+            layoutInArea(line, contentX, lineY,
+                    contentWidth, kMinCellHeight, -0.0,
+                    HPos.LEFT, VPos.TOP)
+        }
+
+        for (i in 0 until vln.size) {
+            for (j in 0 until hln.size) {
+                val cell = cells[i * hln.size + j]
+                val lineX = contentX + i * kMinCellWidth + kTextMargin
+                val lineY = contentY + j * kMinCellHeight
+                layoutInArea(cell, lineX, lineY, kMinCellWidth - 2 * kTextMargin,
+                        kMinCellHeight, 0.0, HPos.LEFT, VPos.CENTER)
+            }
+        }
 
 
         layoutInArea(hsb, 0.0, 0.0, contentWidth - vsb.width, contentHeight, 0.0,
