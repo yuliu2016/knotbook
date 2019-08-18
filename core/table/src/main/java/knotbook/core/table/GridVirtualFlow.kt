e9cdbfd4800540531e10a9e6db85640e2018ba60
yuliu2016 <22801773+yuliu2016@users.noreply.github.com>
2019-08-18 08:41:42 -0400

    add GridVirtualFlow

---
+++ core/table/src/main/java/knotbook/core/table/GridVirtualFlow.kt
@@ -0,0 +1,10 @@
+package knotbook.core.table
+
+/**
+ * Virtual Flow for a grid, based upon [LinearVirtualFlow]
+ */
+@Suppress("unused")
+class GridVirtualFlow {
+    val x = LinearVirtualFlow()
+    val y = LinearVirtualFlow()
+}
