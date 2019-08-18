776b05ded07475021515c1bd113c019fa28c7fc0
yuliu2016 <22801773+yuliu2016@users.noreply.github.com>
2019-08-18 08:08:59 -0400

    add LinearVirtualFlow

--- core/table/src/main/java/knotbook/core/table/LinearVirtualFlow.kt
+++ core/table/src/main/java/knotbook/core/table/LinearVirtualFlow.kt
@@ -17,4 +17,64 @@ package knotbook.core.table
 
 class LinearVirtualFlow {
 
+    // === MIN-MAX DIMENSIONS ===
+
+    var minSize = 0.0
+    var maxSize = 0.0
+
+    // === TOTAL FINITE DIMENSIONS ===
+
+    // The total number of sections to display
+
+    var cells = 0
+        private set
+
+    // The size of individual cells
+
+    var cellSizes = DoubleArray(0)
+
+    // The position of lines separating the
+
+    var positions = DoubleArray(0)
+
+    // The total dimensions of the entire grid
+    // Equal to the sum of the individual dimensions up to the specified size
+
+    var totalSize = 0.0
+        private set
+
+    // === VIRTUAL DIMENSIONS ===
+
+    // The number of lines that need to show on the screen
+
+    var virtualCells = 0
+        private set
+
+    // The position of grid lines
+
+    var virtualPositions = DoubleArray(0)
+
+    // The size of the virtual box
+
+    var clipSize = 0.0
+        private set
+
+    // === VIEW CONFIGURATION ===
+
+    // The zoom factor -- Everything else is invariant to the zoom factor
+    // Must apply it in every calculation to avoid changing other parameters
+
+    var zoomFactor = 1.0
+        private set
+
+    // The current position in [0, 1] representing MIN/MAX
+
+    var scroll = 0.0
+        private set
+
+    // === COMPUTE STATE/MARKERS ===
+
+    // State markers
+    var requiresLayout = false
+        private set
 }
