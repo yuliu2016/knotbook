431dfabbb7dac15d8031260a473cabfded1f80de
yuliu2016 <22801773+yuliu2016@users.noreply.github.com>
2019-08-17 21:04:12 -0400

    remove platform script task from pipelines

---
+++ core/table/src/main/java/knotbook/core/table/LinearVirtualFlow.kt
@@ -0,0 +1,20 @@
+package knotbook.core.table
+
+/**
+ * Pure implementation of VirtualFlow that is independent of cells
+ * or scroll bars; those can be handled in another class
+ *
+ * [LinearVirtualFlow] represents the view in 1D. 2 instances of this
+ * class can be used to construct a virtual flow mechanism for an
+ * actual table/grid
+ *
+ * The class also includes some fine tuned calculations for
+ * JavaFX layout.
+ *
+ * Usage of this class requires calling the right methods after
+ * changing parameters, because it's too complex to listen for changes.
+ */
+
+class LinearVirtualFlow {
+
+}
