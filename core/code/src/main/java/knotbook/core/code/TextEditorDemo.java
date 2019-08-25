4a7fd4e479852ba8485ba92ec3237631f9ec2cdc
yuliu2016 <22801773+yuliu2016@users.noreply.github.com>
2019-08-25 08:59:10 -0400

    update options

--- core/code/src/main/java/knotbook/core/code/TextEditorDemo.java
+++ core/code/src/main/java/knotbook/core/code/TextEditorDemo.java
@@ -11,35 +11,40 @@ import java.awt.*;
  * A simple example showing how to use RSyntaxTextArea to add Java syntax
  * highlighting to a Swing application.
  */
-public class TextEditorDemo{
+public class TextEditorDemo {
 
-   private TextEditorDemo() {
+    private TextEditorDemo() {
 
-      JPanel cp = new JPanel(new BorderLayout());
+        JPanel cp = new JPanel(new BorderLayout());
 
-      RSyntaxTextArea textArea = new RSyntaxTextArea(36, 100);
-      textArea.setSyntaxEditingStyle(SyntaxConstants.SYNTAX_STYLE_PYTHON);
-      textArea.setCurrentLineHighlightColor(Color.white);
-      textArea.setFont(textArea.getFont().deriveFont(16f));
-      textArea.setAntiAliasingEnabled(true);
-      textArea.setCodeFoldingEnabled(true);
+        RSyntaxTextArea textArea = new RSyntaxTextArea(36, 100);
+        textArea.setSyntaxEditingStyle(SyntaxConstants.SYNTAX_STYLE_PROPERTIES_FILE);
+        textArea.setCurrentLineHighlightColor(Color.white);
+        textArea.setAntiAliasingEnabled(true);
+        textArea.setCodeFoldingEnabled(true);
+        textArea.setAutoIndentEnabled(true);
+        textArea.setBracketMatchingEnabled(true);
+        textArea.setPaintTabLines(true);
+        textArea.setTabSize(4);
+        textArea.setTabsEmulated(true);
+        textArea.setCloseCurlyBraces(true);
+        textArea.setMarkOccurrences(true);
 
-      RTextScrollPane sp = new RTextScrollPane(textArea);
-      cp.add(sp);
+        RTextScrollPane sp = new RTextScrollPane(textArea);
+        cp.add(sp);
 
-      JFrame frame = new JFrame();
+        JFrame frame = new JFrame();
 
-      frame.setContentPane(cp);
-      frame.setTitle("Text Editor Demo");
-      frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-      frame.pack();
-      frame.setLocationRelativeTo(null);
-      frame.setVisible(true);
+        frame.setContentPane(cp);
+        frame.setTitle("Text Editor Demo");
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
 
-   }
-
-   public static void launch() {
-      // Start all Swing applications on the EDT.
-      SwingUtilities.invokeLater(TextEditorDemo::new);
-   }
+    public static void launch() {
+        // Start all Swing applications on the EDT.
+        SwingUtilities.invokeLater(TextEditorDemo::new);
+    }
 }
