28bff4e9682fb8a8ba8793e9700653be576ab1f6
yuliu2016 <22801773+yuliu2016@users.noreply.github.com>
2019-09-06 21:17:02 -0400

    update tba-client dependencies

---
+++ modules/kb.core.view/src/main/kotlin/kb/core/view/TBAView.kt
@@ -0,0 +1,9 @@
+package kb.core.view
+
+import javafx.stage.Stage
+
+class TBAView {
+    init {
+        val stage = Stage()
+    }
+}
---
+++ modules/kb.lib.krangl/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    java
+    kotlin("jvm")
+}
+
+dependencies {
+
+    //    compile "org.apache.commons:commons-csv:1.3"
+//    compile "com.univocity:univocity-parsers:2.7.5"
+//    compile 'com.beust:klaxon:0.30'
+//    compile 'me.tongfei:progressbar:0.5.5'
+}
