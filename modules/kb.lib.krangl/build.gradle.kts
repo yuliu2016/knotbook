829b1f8b3f96e735d863cead11e389f98c97180d
yuliu2016 <22801773+yuliu2016@users.noreply.github.com>
2019-09-06 21:24:07 -0400

    set up for krangl

--- modules/kb.lib.krangl/build.gradle.kts
+++ modules/kb.lib.krangl/build.gradle.kts
@@ -3,10 +3,32 @@ plugins {
     kotlin("jvm")
 }
 
-dependencies {
+tasks {
+    compileKotlin {
+        kotlinOptions {
+            freeCompilerArgs = listOf("-Xnew-inference")
+            jvmTarget = "11"
+        }
+    }
+    compileTestKotlin {
+        kotlinOptions {
+            freeCompilerArgs = listOf("-Xnew-inference")
+            jvmTarget = "11"
+        }
+    }
+}
 
-    //    compile "org.apache.commons:commons-csv:1.3"
-//    compile "com.univocity:univocity-parsers:2.7.5"
-//    compile 'com.beust:klaxon:0.30'
-//    compile 'me.tongfei:progressbar:0.5.5'
+tasks.withType<Test> {
+    useJUnitPlatform {
+    }
+}
+
+dependencies {
+    implementation(group = "org.apache.commons", name = "commons-csv", version = "1.6")
+    implementation(group = "com.beust", name = "klaxon", version = "5.0.12")
+
+    testImplementation(kotlin("test"))
+    testImplementation(group = "org.junit.jupiter", name = "junit-jupiter-api", version = "5.5.1")
+    testRuntimeOnly(group = "org.junit.jupiter", name = "junit-jupiter-engine", version = "5.5.1")
+    testRuntimeOnly(group = "org.junit.platform", name = "junit-platform-launcher", version = "1.5.1")
 }
