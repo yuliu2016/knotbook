27a5cc9707477d5b69223d0ff5f39a680ac5480c
yuliu2016 <22801773+yuliu2016@users.noreply.github.com>
2019-09-15 15:04:55 -0400

    rev-347: remove IntColumnKt

--- modules/kb.core.data/src/main/kotlin/kb/core/data/IntColumn.kt
+++
@@ -1,8 +0,0 @@
-package kb.core.data
-
-@Suppress("MemberVisibilityCanBePrivate")
-class IntColumn(override val name: String,
-                val values: Array<Int?>) : DataColumn {
-    override val size: Int
-        get() = values.size
-}
