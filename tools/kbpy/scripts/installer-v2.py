72397babbec2787c52b6703e9399bfd1bd649f7b
yuliu2016 <22801773+yuliu2016@users.noreply.github.com>
2019-09-17 19:20:49 -0400

    add ExtendedEnvBuilder

--- tools/kbpy/scripts/installer-v2.py
+++ tools/kbpy/scripts/installer-v2.py
@@ -1,11 +1,15 @@
-from urllib.request import urlopen
+from urllib.request import urlopen, urlretrieve
+from urllib.parse import urlparse
 from typing import Iterable
+from subprocess import Popen, PIPE
+from threading import Thread
 import json
 import sys
 import platform
 import tarfile
 import zipfile
 import os
+import os.path
 import venv
 import time
 
@@ -23,6 +27,204 @@ if not is_64bit:
     print("This script must run on a 64-bit architecture")
     input()
     sys.exit(1)
+
+
+# See https://docs.python.org/3/library/venv.html
+class ExtendedEnvBuilder(venv.EnvBuilder):
+    """
+    This builder installs setuptools and pip so that you can pip or
+    easy_install other packages into the created virtual environment.
+
+    param nodist: If True, setuptools and pip are not installed into the
+                   created virtual environment.
+    param nopip: If True, pip is not installed into the created
+                  virtual environment.
+    param progress: If setuptools or pip are installed, the progress of the
+                     installation can be monitored by passing a progress
+                     callable. If specified, it is called with two
+                     arguments: a string indicating some progress, and a
+                     context indicating where the string is coming from.
+                     The context argument can have one of three values:
+                     'main', indicating that it is called from virtualize()
+                     itself, and 'stdout' and 'stderr', which are obtained
+                     by reading lines from the output streams of a subprocess
+                     which is used to install the app.
+
+                     If a callable is not specified, default progress
+                     information is output to sys.stderr.
+    """
+
+    def __init__(self, *args, **kwargs):
+        self.nodist = kwargs.pop('nodist', False)
+        self.nopip = kwargs.pop('nopip', False)
+        self.progress = kwargs.pop('progress', None)
+        self.verbose = kwargs.pop('verbose', False)
+        super().__init__(*args, **kwargs)
+
+    def post_setup(self, context):
+        """
+        Set up any packages which need to be pre-installed into the
+        virtual environment being created.
+
+        param context: The information for the virtual environment
+                        creation request being processed.
+        """
+        os.environ['VIRTUAL_ENV'] = context.env_dir
+        if not self.nodist:
+            self.install_setuptools(context)
+        # Can't install pip without setuptools
+        if not self.nopip and not self.nodist:
+            self.install_pip(context)
+
+    def reader(self, stream, context):
+        """
+        Read lines from a subprocess' output stream and either pass to a progress
+        callable (if specified) or write progress information to sys.stderr.
+        """
+        progress = self.progress
+        while True:
+            s = stream.readline()
+            if not s:
+                break
+            if progress is not None:
+                progress(s, context)
+            else:
+                if not self.verbose:
+                    sys.stderr.write('.')
+                else:
+                    sys.stderr.write(s.decode('utf-8'))
+                sys.stderr.flush()
+        stream.close()
+
+    def install_script(self, context, name, url):
+        _, _, path, _, _, _ = urlparse(url)
+        fn = os.path.split(path)[-1]
+        binpath = context.bin_path
+        distpath = os.path.join(binpath, fn)
+        # Download script into the virtual environment's binaries folder
+        urlretrieve(url, distpath)
+        progress = self.progress
+        if self.verbose:
+            term = '\n'
+        else:
+            term = ''
+        if progress is not None:
+            progress('Installing %s ...%s' % (name, term), 'main')
+        else:
+            sys.stderr.write('Installing %s ...%s' % (name, term))
+            sys.stderr.flush()
+        # Install in the virtual environment
+        args = [context.env_exe, fn]
+        p = Popen(args, stdout=PIPE, stderr=PIPE, cwd=binpath)
+        t1 = Thread(target=self.reader, args=(p.stdout, 'stdout'))
+        t1.start()
+        t2 = Thread(target=self.reader, args=(p.stderr, 'stderr'))
+        t2.start()
+        p.wait()
+        t1.join()
+        t2.join()
+        if progress is not None:
+            progress('done.', 'main')
+        else:
+            sys.stderr.write('done.\n')
+        # Clean up - no longer needed
+        os.unlink(distpath)
+
+    def install_setuptools(self, context):
+        """
+        Install setuptools in the virtual environment.
+
+        param context: The information for the virtual environment
+                        creation request being processed.
+        """
+        url = 'https://bitbucket.org/pypa/setuptools/downloads/ez_setup.py'
+        self.install_script(context, 'setuptools', url)
+        # clear up the setuptools archive which gets downloaded
+        pred = lambda o: o.startswith('setuptools-') and o.endswith('.tar.gz')
+        files = filter(pred, os.listdir(context.bin_path))
+        for f in files:
+            f = os.path.join(context.bin_path, f)
+            os.unlink(f)
+
+    def install_pip(self, context):
+        """
+        Install pip in the virtual environment.
+
+        param context: The information for the virtual environment
+                        creation request being processed.
+        """
+        url = 'https://raw.github.com/pypa/pip/master/contrib/get-pip.py'
+        self.install_script(context, 'pip', url)
+
+
+def main(args=None):
+    compatible = True
+    if not hasattr(sys, 'base_prefix'):
+        compatible = False
+    if not compatible:
+        raise ValueError('This script is only for use with '
+                         'Python 3.3 or later')
+    else:
+        import argparse
+
+        parser = argparse.ArgumentParser(prog=__name__,
+                                         description='Creates virtual Python '
+                                                     'environments in one or '
+                                                     'more target '
+                                                     'directories.')
+        parser.add_argument('dirs', metavar='ENV_DIR', nargs='+',
+                            help='A directory in which to create the'
+                                 'virtual environment.')
+        parser.add_argument('--no-setuptools', default=False,
+                            action='store_true', dest='nodist',
+                            help="Don't install setuptools or pip in the "
+                                 "virtual environment.")
+        parser.add_argument('--no-pip', default=False,
+                            action='store_true', dest='nopip',
+                            help="Don't install pip in the virtual "
+                                 "environment.")
+        parser.add_argument('--system-site-packages', default=False,
+                            action='store_true', dest='system_site',
+                            help='Give the virtual environment access to the '
+                                 'system site-packages dir.')
+        if os.name == 'nt':
+            use_symlinks = False
+        else:
+            use_symlinks = True
+        parser.add_argument('--symlinks', default=use_symlinks,
+                            action='store_true', dest='symlinks',
+                            help='Try to use symlinks rather than copies, '
+                                 'when symlinks are not the default for '
+                                 'the platform.')
+        parser.add_argument('--clear', default=False, action='store_true',
+                            dest='clear', help='Delete the contents of the '
+                                               'virtual environment '
+                                               'directory if it already '
+                                               'exists, before virtual '
+                                               'environment creation.')
+        parser.add_argument('--upgrade', default=False, action='store_true',
+                            dest='upgrade', help='Upgrade the virtual '
+                                                 'environment directory to '
+                                                 'use this version of '
+                                                 'Python, assuming Python '
+                                                 'has been upgraded '
+                                                 'in-place.')
+        parser.add_argument('--verbose', default=False, action='store_true',
+                            dest='verbose', help='Display the output '
+                                                 'from the scripts which '
+                                                 'install setuptools and pip.')
+        options = parser.parse_args(args)
+        if options.upgrade and options.clear:
+            raise ValueError('you cannot supply --upgrade and --clear together.')
+        builder = ExtendedEnvBuilder(system_site_packages=options.system_site,
+                                     clear=options.clear,
+                                     symlinks=options.symlinks,
+                                     upgrade=options.upgrade,
+                                     nodist=options.nodist,
+                                     nopip=options.nopip,
+                                     verbose=options.verbose)
+        for d in options.dirs:
+            builder.create(d)
 
 
 def make_venv():
@@ -57,7 +259,7 @@ def download_files(target: "str"):
 
     try:
         u = urlopen(download_url)
-        block_size = 2 ** 20  # 1 block per Mb
+        block_size = 2 ** 18  # 4 blocks per Mb
         with open("tempfile", 'wb') as f:
             file_size_downloaded = 0
             while True:
@@ -68,7 +270,7 @@ def download_files(target: "str"):
                 f.write(buffer)
 
                 mb = file_size_downloaded / 1000000
-                print(f"Application File: Downloaded {mb:.2f}MB")
+                print(f"Application Image: Downloaded {mb:.2f}MB")
     except Exception:
         import traceback
 
@@ -107,7 +309,7 @@ def extract_files(target: "str"):
         print(f"Done Extracting in {time.time() - s} seconds")
 
     else:
-        print("No files...")
+        print("Target file is not found")
 
     # tar = tarfile.open(fname, "r:gz")
     # tar.extractall()
