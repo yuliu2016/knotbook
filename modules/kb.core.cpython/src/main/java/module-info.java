505ff2987592d2d0edd435f278c24171e4d007f1
yuliu2016 <22801773+yuliu2016@users.noreply.github.com>
2019-09-20 20:24:26 -0400

    rev-376: remove cpy module

--- modules/kb.core.cpython/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module kb.core.cpython {
-    requires org.bytedeco.javacpp;
-    requires org.bytedeco.cpython;
-//    requires org.bytedeco.cpython.windows.x86_64;
-//    requires org.bytedeco.cpython.macosx.x86_64;
-//    requires org.bytedeco.cpython.linux.x86_64;
-
-    exports kb.core.cpython;
-}
