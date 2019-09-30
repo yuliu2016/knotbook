25224377a56dfb4559bfa19c84250d25360f561a
yuliu2016 <22801773+yuliu2016@users.noreply.github.com>
2019-09-29 20:26:18 -0400

    add ServerService

---
+++ modules/kb.core.server/src/main/kotlin/kb/core/server/ServerService.kt
@@ -0,0 +1,31 @@
+package kb.core.server
+
+import kb.service.api.Service
+import kb.service.api.ServiceContext
+import kb.service.api.ServiceMetadata
+
+class ServerService : Service {
+
+    private val metadata = ServiceMetadata()
+    private val server = Server()
+
+
+    init {
+        metadata.packageName = "kb.core.server"
+        metadata.packageVersion = "3.0"
+    }
+
+
+    override fun getMetadata(): ServiceMetadata {
+        return metadata
+    }
+
+    override fun launch(context: ServiceContext) {
+        server.server.start()
+    }
+
+    override fun terminate(): Boolean {
+        server.server.stop(3)
+        return false
+    }
+}
