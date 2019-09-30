444a94b2f05c65943b27eccd95a11e710ff76532
yuliu2016 <22801773+yuliu2016@users.noreply.github.com>
2019-09-29 21:22:48 -0400

    add BufferedImageLuminanceSource

--- modules/kb.core.camera.fx/src/main/java/kb/core/camera/fx/KnotCamera.java
+++ modules/kb.core.camera.fx/src/main/java/kb/core/camera/fx/KnotCamera.java
@@ -6,7 +6,6 @@ import com.google.zxing.BinaryBitmap;
 import com.google.zxing.MultiFormatReader;
 import com.google.zxing.NotFoundException;
 import com.google.zxing.Result;
-import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
 import com.google.zxing.common.HybridBinarizer;
 import javafx.animation.AnimationTimer;
 import javafx.beans.property.*;
--- modules/kb.core.camera.fx/src/main/java/module-info.java
+++ modules/kb.core.camera.fx/src/main/java/module-info.java
@@ -1,6 +1,5 @@
 module kb.core.camera.fx {
     requires com.google.zxing;
-    requires com.google.zxing.javase;
     requires webcam.capture;
     requires javafx.controls;
     requires java.desktop;
